74162893f6dd3e485cdaa4b4244087069554920a
Monisha R <monishar729735@gmail.com>
2024-09-15 19:13:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <HomeCards />
+    <JobListings />
+    <ViewAllJobs />
+    </>
+  )
+}
+
+export default App
