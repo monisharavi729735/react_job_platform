a190be90a322429729ae1e856e824ee84c084605
Monisha R <monishar729735@gmail.com>
2024-09-20 19:47:03 +0530

    edit job

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -184,11 +184,6 @@ const EditJobPage = ({updateJobSubmit}) => {
               >
                 Save Changes
               </button>
-              <button
-                className="bg-red-500 mt-2 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
-              >
-                Cancel
-              </button>
             </div>
           </form>
         </div>
